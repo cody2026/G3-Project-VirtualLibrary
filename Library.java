0260a36cc2c33c2f2c6f07c900623eb1a0121d1d
indy2026 <innguyen@iu.edu>
2026-04-07 17:24:46 -0400

    Add files via upload
    Skeleton version of the main controller class "Library" that manages books and users.

---
+++ Library.java
@@ -0,0 +1,76 @@
+/**
+ * Team Name: G3
+ * Project Name: Virtual Library Management System
+ * Author: Indy Nguyen
+ * Date:  04/07/2026
+ * Class: Library
+ * Description: Main controller class that manages books and users.
+ * Handles all operations such as adding, searching, checkout, and return.
+ */
+
+package team;
+
+import java.util.ArrayList;
+
+public class Library {
+    
+    // attributes: list of books and users
+    private ArrayList<Book> books;
+    private ArrayList<UserAccount> users;
+    
+    // constructor initializes lists
+    public Library() {
+        
+    }
+    
+    // add new book
+    public void addBook() {
+        
+    }
+    
+    // add new user
+    public void addUser() {
+        
+    }
+    
+    // find book by ID
+    public Book findBookById() {
+        
+    }
+    
+    // find user by ID
+    public UserAccount findUserById() {
+        
+    }
+    
+    // search books by title
+    public ArrayList<Book> searchBooksByTitle() {
+        
+    }
+    
+    // checkout book to user
+    public String checkoutBook() {
+        
+    }
+    
+    // return book from user
+    public String returnBook() {
+        
+    }
+    
+    // display all books
+    public String displayAllBooks() {
+        
+    }
+    
+    // display available books
+    public String displayAvailableBooks() {
+        
+    }
+    
+    // display all users
+    public String displayAllUsers() {
+        
+    }
+
+}
