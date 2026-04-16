41e5fc2e0924bce10b0d19efb45e7472d1fb1539
tlewis1031 <tlewis1031@gmail.com>
2026-04-16 18:01:23 -0400

    Book.java

---
+++ Book.java
@@ -0,0 +1,59 @@
+/** 
+ * Team G3
+ * Project  Library Management System
+ * Author    Theresa Lewis
+ * Class Book
+ * Tracks books checked out
+ */
+
+package Team;
+
+
+public class Book {
+	
+    // Attributes 
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean checkedOut;
+
+    // Constructor to initialize a new book object
+    public Book() {
+    	
+        }
+
+    // Getter methods to retrieve book details
+    public int getBookId() {
+    	
+	}
+	
+    public String getTitle() {
+    	
+    }
+    
+    public String getAuthor() {
+    	
+    }
+        
+    public boolean isCheckedOut() {
+    	
+    }
+
+    // Method to check out the book 
+    public void checkoutBook() {
+       
+    }
+
+    // Method to return the book 
+    public void returnBook() {
+    	        
+    }
+
+    // Overridden toString method 
+    @Override
+    public String toString() {
+       
+    }
+}
+
+	
