1b0ee140c0098981b36a4a3c627cb3ef18feceaf
tlewis1031 <tlewis1031@gmail.com>
2026-04-23 17:52:42 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,59 +1,65 @@
-/** 
- * Team G3
- * Project  Library Management System
- * Author    Theresa Lewis
- * Class Book
- * Tracks books checked out
- */
-
 package Team;
-
+/**
+* Team G3
+* Project Library Management System
+* Author Theresa Lewis
+* Class Book
+* Tracks books checked out
+*/
 
 public class Book {
 	
-    // Attributes 
+    // Attributes
     private int bookId;
     private String title;
     private String author;
     private boolean checkedOut;
 
     // Constructor to initialize a new book object
-    public Book() {
-    	
-        }
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.checkedOut = false; 
+    }
 
     // Getter methods to retrieve book details
-    public int getBookId() {
-    	
-	}
-	
-    public String getTitle() {
-    	
+    public int getBookId() { 
+    	return bookId; 
     }
-    
+    public String getTitle() { 
+       	return title; 
+    }
     public String getAuthor() {
-    	
+    	return author; 
     }
-        
     public boolean isCheckedOut() {
-    	
+        return checkedOut;
     }
 
-    // Method to check out the book 
+    // Library Methods
     public void checkoutBook() {
-       
+        if (!checkedOut) {
+            checkedOut = true;
+            System.out.println("The book '" + title + "' has been checked out.");
+        } else {
+            System.out.println("Sorry, '" + title + "' is already checked out.");
+        }
     }
 
-    // Method to return the book 
     public void returnBook() {
-    	        
+        if (checkedOut) {
+            checkedOut = false;
+            System.out.println("The book '" + title + "' has been returned.");
+        } else {
+            System.out.println("The book '" + title + "' was not checked out.");
+        }
     }
 
-    // Overridden toString method 
     @Override
     public String toString() {
-       
+        String status = checkedOut ? "Checked Out" : "Available";
+        return "Book ID: " + bookId + " | Title: " + title + 
+               " | Author: " + author + " | Status: " + status;
     }
 }
-
-	
